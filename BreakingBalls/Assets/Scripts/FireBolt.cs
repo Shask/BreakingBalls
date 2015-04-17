a0776376568eb37eef0727460a1354b15325138d
Amicel Anthony <anthony.amicel1@uqac.ca>
2015-04-17 12:14:14 -0400

    Boule de feu, non fonctionnelle encore...

--- BreakingBalls/Assets/Scripts/FireBolt.cs
+++ BreakingBalls/Assets/Scripts/FireBolt.cs
@@ -4,17 +4,31 @@ using System.Collections;
 public class FireBolt : MonoBehaviour {
 
 	private Vector3 target;
-	private float speed = 10f;
+	private float speed = 0.15f;
+
+	//public Transform _destination;
+	public float _speed;
+	private Vector3 maposition;
+	
+	public void Initialize(Transform destination, float vitesse, Vector3 depart)
+	{
+
+		target = destination.position;
+		speed = vitesse;
+		maposition = depart;
+		transform.position = maposition;
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		target = new Vector3 (2, 2, 0);
+		transform.position = new Vector3 (0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (0, 0, 2));
-		transform.Translate (target * speed);
+		//transform.Rotate (new Vector3 (0, 0, 2));
+		transform.Translate (target * speed * Time.deltaTime);
 	}
 
 	void OnTriggerEnter(Collider other) {
--- BreakingBalls/Assets/Scripts/Firefox.cs
+++ BreakingBalls/Assets/Scripts/Firefox.cs
@@ -6,10 +6,18 @@ public class Firefox : MonoBehaviour {
 	private float speed = 0.15f;
 	private Vector3 translateur;
 	private SphereCollider spCol;
+
+	private float _nextShotInSecond;
+	private FireBolt projectile;
+	private Transform destination;
 	// Use this for initialization
 	void Start () {
 		translateur = new Vector3 (0, 5, 0);
 		spCol = this.GetComponent<SphereCollider> ();
+
+		destination.position = new Vector3 (0, 0, 0);
+		//destination.transform.position = new Vector3 (5, 5, 0);
+		_nextShotInSecond = 0.1f;
 	}
 	
 	// Update is called once per frame
@@ -22,6 +30,13 @@ public class Firefox : MonoBehaviour {
 		}
 		transform.Translate (translateur * speed * Time.deltaTime);
 		//spCol.transform.Translate (new Vector3 (0, -3, 0) * speed*4 * Time.deltaTime);
+
+		if ((_nextShotInSecond -= Time.deltaTime) > 0)
+			return;
+
+		_nextShotInSecond = 50f;
+		bolt = (FireBolt)Instantiate(projectile,transform.position,transform.rotation);
+		bolt.Initialize (destination, 0.25f, transform.position);
 	}
 
 	void OnTriggerEnter(Collider other) {
