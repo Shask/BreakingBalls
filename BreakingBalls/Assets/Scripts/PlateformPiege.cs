6e964411579b16c0feace84b70f7283e34790a6a
Amicel Anthony <anthony.amicel1@uqac.ca>
2015-04-19 16:21:42 -0400

    new Scene

--- BreakingBalls/Assets/Scripts/PlateformPiege.cs
+++ BreakingBalls/Assets/Scripts/PlateformPiege.cs
@@ -9,8 +9,23 @@ public class PlateformPiege : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		p = gameObject.GetComponentInChildren<Piege> ().gameObject;
+		resizeCollider ();
 	}
 	
+	void resizeCollider()
+	{
+		BoxCollider boxCollider;
+		Transform image;
+		SpriteRenderer srImage;
+		
+		boxCollider = transform.GetComponent<BoxCollider>();
+		image = transform.GetChild (0);
+		srImage = image.GetComponent<SpriteRenderer>();
+		//transform.position = image.position;
+		
+		boxCollider.size = new Vector3(srImage.bounds.size.x / transform.localScale.x, srImage.bounds.size.y / transform.localScale.y, boxCollider.size.z);
+		boxCollider.center = new Vector3((image.position.x - transform.position.x) / transform.localScale.x, (image.position.y - transform.position.y) / transform.localScale.y, boxCollider.center.z);
+	}
 	// Update is called once per frame
 	void Update () {
 		
