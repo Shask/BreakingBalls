511928a1f07b8acfbfd8c15f7529b032d22a34e1
Steven Fougeron <steven.fougeron@gmail.com>
2015-04-20 04:09:33 -0400

    Debut de scene d'intro,
    Ajout de 2 type de personnage,
    
    Correction de bug et Equilibrage des items

---
+++ BreakingBalls/Assets/Scripts/IntroCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroCamera : MonoBehaviour {
+
+	private Animation anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+		anim.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
